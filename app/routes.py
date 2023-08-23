e8fbcbb847402c386d7d11aedb4363b3c73d6d0c
Bryan050 <bryanchicaiza14@gmail.com>
2023-08-23 05:57:57 -0500

    Verificar que el usuario está dentro del campus agregado

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from . import signature_handler
+from . import geolocation_handler
 
 api = Blueprint('api', __name__)
 
@@ -8,3 +9,10 @@ def registrar_firma():
     signature_data = request.json 
     result = signature_handler.save_signature(signature_data)
     return jsonify(result)
+
+@api.route('/verificar_ubicacion', methods=['POST'])
+def verificar_ubicacion():
+    data = request.get_json()
+    result = geolocation_handler.check_location(data)
+    return result
+
