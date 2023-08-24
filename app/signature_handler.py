b1134552fc1d0a75079e7e6f4c8742a602485061
anthony unda reinoso <anthonyureinoso@gmail.com>
2023-08-23 20:54:03 -0500

    Importacion de modulos y manejo de excepciones

--- app/signature_handler.py
+++ app/signature_handler.py
@@ -1,6 +1,12 @@
 from config import db
+from pymongo.errors import PyMongoError
 
 def save_signature(signature_data):
-    print(signature_data)
-    db.signatures.insert_one(signature_data)
-    return {'message': 'Firma registrada exitosamente'}
+    try:
+        return {
+            
+        }
+    except PyMongoError as e:
+        return {
+
+        }
