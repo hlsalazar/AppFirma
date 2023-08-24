af2781f3d89c162eb358d06848ab1fc1df275051
anthony unda reinoso <anthonyureinoso@gmail.com>
2023-08-23 20:57:36 -0500

    Validacion de datos y retorno de respuestas

--- app/signature_handler.py
+++ app/signature_handler.py
@@ -3,10 +3,17 @@ from pymongo.errors import PyMongoError
 
 def save_signature(signature_data):
     try:
+        # Validar signature_data aquí si es necesario
+        print(signature_data)
+        db.signatures.insert_one(signature_data)
         return {
-            
+            'success': True,
+            'message': 'Firma registrada exitosamente',
+            'data': signature_data  # Opcionalmente, podrías devolver los datos insertados
         }
     except PyMongoError as e:
         return {
-
+            'success': False,
+            'message': 'Error al registrar la firma',
+            'error': str(e)  # Devuelve el mensaje de error específico
         }
